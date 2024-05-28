c53e63d606da361a8fc0bf187386925a97076273
Peter Phan <100747299+speeDy167@users.noreply.github.com>
2024-05-28 08:36:09 +0700

    Update LoginController.java

--- src/main/java/com/reljicd/controller/LoginController.java
+++ src/main/java/com/reljicd/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.reljicd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.Map;
 
 @Controller
 public class LoginController {
